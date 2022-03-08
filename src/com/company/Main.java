6ec97762b59ae6771b2a6284295d12e6b5633e33
Erzhig1t <erzhigitfcb@gmail.com>
2022-03-08 22:52:28 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Triangle triangle = new Triangle();
+        triangle.b = 4;
+        triangle.a = 3;
+        triangle.c = 6;
+        triangle.setArea();
+    }
+
+}
+
+
---
+++ src/com/company/Triangle.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Triangle {
+    int a;
+    int b;
+    int c;
+    int area;
+    void setArea(){
+        System.out.println((a+b+c)/2);
+    }
+}
